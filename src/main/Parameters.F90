3d5f2b312c07e007ce48d1278c1eb234e15b014e
Sayop Kim <kim.sayop@gmail.com>
2015-09-14 01:06:01 -0400

    recreated main source files

---
+++ src/main/Parameters.F90
@@ -0,0 +1,16 @@
+!> \file parameters.F90
+!> \author Sayop Kim
+!> \brief Provides parameters and physical constants for use throughout the
+!! code.
+MODULE Parameters_m
+   INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(8)
+   ! Number of total elements in state vector
+   INTEGER, PARAMETER :: nelm = 4
+
+   CHARACTER(LEN=10), PARAMETER :: CODE_VER_STRING = "V.001.001"
+   REAL(KIND=wp), PARAMETER :: PI = 3.14159265358979323846264338_wp
+   !> Universal gas constant [J/(kmol.K)]
+   REAL(KIND=wp), PARAMETER :: Ru = 8314.4621_wp
+
+END MODULE Parameters_m
+
